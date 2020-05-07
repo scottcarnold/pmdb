9332765248874e3eed9b3e8cdcd267f9c73f5075
Scott Arnold <scottcarnold@gmail.com>
2020-05-07 11:35:12 -0500

    Get basic properties system set up.

---
+++ pmdb/src/main/java/org/xandercat/pmdb/config/AppConfig.java
@@ -0,0 +1,25 @@
+package org.xandercat.pmdb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.SystemPropertyUtils;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public Resource propertiesResource() {
+		String propertiesLocation = SystemPropertyUtils.resolvePlaceholders("environment/pmdb_${pmdb.environment}.properties");
+		return new ClassPathResource(propertiesLocation);
+	}
+	
+	@Bean
+	public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(Resource propertiesResource) {
+		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+		propertySourcesPlaceholderConfigurer.setLocation(propertiesResource);
+		return propertySourcesPlaceholderConfigurer;
+	}
+}
--- pmdb/src/main/java/org/xandercat/pmdb/controller/HomeController.java
+++ pmdb/src/main/java/org/xandercat/pmdb/controller/HomeController.java
@@ -1,5 +1,6 @@
 package org.xandercat.pmdb.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	@Value("${pmdb.environment.name}")
+	private String environment;
+	
 	@GetMapping("/home")
 	public String test(Model model) {
-		model.addAttribute("message", "You are now logged in");
+		model.addAttribute("message", "You are now logged in with the " + environment + " environment.");
 		return "home";
 	}
 }
